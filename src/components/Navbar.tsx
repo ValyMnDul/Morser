1ee2cc0cefbdbbc39003bced3d1f21fe02571aa5
ValyMnDul <mindrilavasilevalentin@gmail.com>
2025-11-15 21:22:07 +0200

    Enhance Navbar layout and styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,20 +4,40 @@ export default function Navbar(){
 
     return(
         <nav
-        className="w-full h-15"
+        className="w-full h-18 flex justify-evenly items-center"
         >
-            <div>
-
+            <div
+            className="text-zinc-100 text-2xl"
+            >
+                LOGO
             </div>
 
-            <div>
+            <div
+            className="flex gap-x-5 font-mono"
+            >
                 <Link
                 className="text-zinc-100 text-2xl"
                 href='/'
                 >
                     Decoder | Encoder
                 </Link>
+
+                <Link 
+                className="text-zinc-100 text-2xl"
+                href='/codes'
+                >
+                    Codes
+                </Link>
+
+                <Link
+                className="text-zinc-100 text-2xl"
+                href='/about'
+                >
+                    About
+                </Link>
+
             </div>
+
         </nav>
     )
 }
