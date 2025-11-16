f18adfc0a7bc34b19837dd641b06419a9ddcf77e
ValyMnDul <mindrilavasilevalentin@gmail.com>
2025-11-16 10:14:08 +0200

    Add not-found.tsx file;Delete useless svgs;Update Navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,36 +1,45 @@
 import Link from "next/link"
+import Image from "next/image"
 
 export default function Navbar(){
 
     return(
         <nav
-        className="w-full h-18 flex justify-evenly items-center"
+        className="w-full h-20 flex justify-evenly items-center"
         >
             <div
             className="text-zinc-100 text-2xl"
             >
-                LOGO
+                <Image
+                src='vercel.svg'
+                alt="Logo"
+                width={30}
+                height={30}
+                ></Image>
             </div>
 
             <div
             className="flex gap-x-5 font-mono"
             >
                 <Link
-                className="text-zinc-100 text-2xl"
+                className="text-2xl text-zinc-100 border-b-2 border-transparent 
+                transition-colors hover:border-yellow-200"
                 href='/'
                 >
-                    Decoder | Encoder
+                    Decoder|Encoder
                 </Link>
 
                 <Link 
-                className="text-zinc-100 text-2xl"
+                className="text-zinc-100 text-2xl border-b-2 border-transparent 
+                transition-colors hover:border-yellow-200"
                 href='/codes'
                 >
                     Codes
                 </Link>
 
                 <Link
-                className="text-zinc-100 text-2xl"
+                className="text-zinc-100 text-2xl border-b-2 border-transparent 
+                transition-colors hover:border-yellow-200"
                 href='/about'
                 >
                     About
