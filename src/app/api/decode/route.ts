7f1703b6916e1092634ae2c2cf94c2072ec9457b
ValyMnDul <mindrilavasilevalentin@gmail.com>
2025-11-25 11:54:30 +0200

    Add decode functionality

--- src/app/api/decode/route.ts
+++ src/app/api/decode/route.ts
@@ -1,11 +1,19 @@
 import { NextResponse, NextRequest } from "next/server";
+import decode from '@/lib/decode.json'
 
 export const POST = async (req:NextRequest) => {
+
+    const decodeLib:Record<string,string> = decode;
 
     const text:string = (await req.json()).text;
     console.log("\"" + text + "\"");
 
-    const outputFromTheServer =1
+    const morseChars = text.split('  ');
+    console.log(morseChars)
+
+    const outputFromTheServer = morseChars.map((char) => (
+        decodeLib[char] !== undefined ? decodeLib[char] : "<Unknown>"
+    )).join("");
 
     return NextResponse.json({outputFromTheServer},{status:200});
 }
